b5122ed7ec39aa52896e4957fbaa86dd1210302a
Christopher Orr <chris@orr.me.uk>
2018-03-07 01:59:18 +0100

    Add global config for setting Slack OAuth 2.0 client ID and secret.

---
+++ src/main/kotlin/org/jenkinsci/plugins/slacktrigger/SlackGlobalConfiguration.kt
@@ -0,0 +1,32 @@
+package org.jenkinsci.plugins.slacktrigger
+
+import hudson.Extension
+import hudson.util.Secret
+import jenkins.model.GlobalConfiguration
+import org.kohsuke.stapler.DataBoundSetter
+
+@Extension
+class SlackGlobalConfiguration : GlobalConfiguration() {
+
+    var clientId: String? = null
+        @DataBoundSetter set(value) {
+            field = value
+            save()
+        }
+
+    var clientSecret: Secret? = null
+        @DataBoundSetter set(value) {
+            field = value
+            save()
+        }
+
+    companion object {
+        fun get() = GlobalConfiguration.all().get(SlackGlobalConfiguration::class.java)!!
+    }
+
+    init {
+        // When Jenkins is restarted, load any saved configuration from disk
+        load()
+    }
+
+}
